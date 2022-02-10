b42eee46e3ff4e53f03afd20927c19477216cb1a
amkrtychian <artur.mkr@gmail.com>
2022-02-10 16:30:19 +0200

    git init

---
+++ main.py
@@ -0,0 +1,19 @@
+import json
+from datetime import datetime
+
+
+def update_json():
+    json_file = open("data.json", "r")
+    json_object = json.load(json_file)
+    json_file.close()
+
+    json_object["id"] += 1
+    json_object["updated_at"] = str(datetime.now())
+
+    a_file = open("data.json", "w")
+    json.dump(json_object, a_file)
+    a_file.close()
+
+
+if __name__ == '__main__':
+    update_json()
