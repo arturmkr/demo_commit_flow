82137d45cec4764ceb44a6ef0633da80c23c257f
amkrtychian <artur.mkr@gmail.com>
2022-03-28 14:12:25 +0300

    check for pr

--- main.py
+++ main.py
@@ -15,5 +15,19 @@ def update_json():
     a_file.close()
 
 
+def update_text():
+    json_file = open("text.json", "r")
+    json_object = json.load(json_file)
+    json_file.close()
+
+    json_object["id"] += 1
+    json_object["updated_at"] = str(datetime.now())
+
+    a_file = open("text.json", "w")
+    json.dump(json_object, a_file)
+    a_file.close()
+
+
 if __name__ == "__main__":
     update_json()
+    update_text()
