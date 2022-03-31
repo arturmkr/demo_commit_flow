90e1248efe98ba44dcc6814329c27353f6a0a6a7
amkrtychian <artur.mkr@gmail.com>
2022-03-31 23:22:53 +0300

    my demo test

--- script.py
+++ script.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-message", action="store", nargs=1)
     args = vars(parser.parse_args())
-    message = args["message"]
+    message = args["message"][0]
     accepted_tokens = ['dev_auth_token_value', 'prod_auth_token_value']
     if message in accepted_tokens:
         print("auth passed")
