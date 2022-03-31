acc794f4b260c4d34b868480a28fdc426da0647f
amkrtychian <artur.mkr@gmail.com>
2022-03-31 23:06:53 +0300

    my demo test

--- script.py
+++ script.py
@@ -1,20 +1,8 @@
-import sys
-import re
-import os
 import argparse
-import warnings
-from collections import OrderedDict
 
-sys.path.append('.')
-sys.path.append("../../expr_automation")
-from scripts.rule_loader.logica_utils.logica_utils import predicate_fields
-
-
-print("Hello")
-
-
-def say_hello(message: str):
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-message", action="store", nargs=1)
+    args = vars(parser.parse_args())
+    message = args["message"]
     print(message)
-
-
-say_hello("How are you")
